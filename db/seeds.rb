a238412a97b24006e551effd8e77ae4b9212fad8
lokideos <archmage09@gmail.com>
2021-09-05 00:06:54 +0300

    Add published to posts and update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Post.create ([{ title: 'Ruby is Awesome', body: 'Ruby is created by Matz'} ])
+posts = Post.create([
+                      { title: 'Ruby is Awesome', body: 'Ruby is created by Matz', published: true},
+                      { title: 'Rails is Amazing', body: 'Rails was created by DHH', published: true},
+                      { title: 'JIT is Amazing', body: 'Ruby JIT is mainly developed by Takashi', published: false},
+                    ])
