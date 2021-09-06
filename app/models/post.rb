28f35ea508fef9f1c6460191115d87dbef75fbd9
lokideos <archmage09@gmail.com>
2021-09-06 19:47:15 +0300

    [Railscasts] #6 - Shortcut Blocks with Symbol to_proc

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
+  class << self
+
+    # Railscasts 6 - Symbol to_proc
+    def all_titles
+      all.collect(&:title)
+    end
+  end
+
   # Railscasts 2 - Dynamic find_by Methods
   def last_published
     Post.find_by_published(true, :order => 'created_at DESC')
